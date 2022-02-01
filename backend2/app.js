3501d85a343030f708ce91082b1d09ab73e944f6
Carlos <carlosolivares9@gmail.com>
2022-02-01 00:13:14 -0600

    changes

---
+++ backend2/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+
+const path = require('path');
+
+const { json, urlencoded } = express
+const app = express()
+
+const host = process.env.IP  || '0.0.0.0'
+const port = process.env.PORT || 3000
+
+app.use(json())
+app.use(urlencoded({ extended: false }))
+
+const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
+app.use(cors(corsOptions))
+
+
+app.use('/home',(req,res) => { res.sendFile(path.join(__dirname+'/src/html/index.html')); })
+app.use('/', (req, res) => { res.send(`Success`);})
+
+app.listen(port,host, () => { console.log(`Server listening on port ${port} in the host ${host}`); })
