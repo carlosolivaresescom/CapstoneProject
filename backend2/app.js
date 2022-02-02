f27553b93d5248f5e71559a8573f15c84ef43c4e
Carlos <carlosolivares9@gmail.com>
2022-02-02 11:16:46 -0600

    force eslint fail

--- backend2/app.js
+++ backend2/app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 
-const path = require('path');
+const forceEslintFail = require('cors')
 
 const { json, urlencoded } = express
 const app = express()
 
-const host = process.env.IP  || '0.0.0.0'
-const port = process.env.PORT || 3000
+const host = '0.0.0.0'
+const port = 3000
 
 app.use(json())
 app.use(urlencoded({ extended: false }))
@@ -15,8 +15,6 @@ app.use(urlencoded({ extended: false }))
 const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
 app.use(cors(corsOptions))
 
-
-app.use('/home',(req,res) => { res.sendFile(path.join(__dirname+'/src/html/index.html')); })
 app.use('/', (req, res) => { res.send(`Success`);})
 
 app.listen(port,host, () => { console.log(`Server listening on port ${port} in the host ${host}`); })
